9cc9590d3d82b055cf49f98faa2b4ef4f7c6660f
liuxicai <liuxicai@dianxiaohuo.net>
2018-03-23 08:39:16 +0800

    develop:薪资类型

---
+++ src/main/java/com/lxc/service/compensation/stipendtype/impl/StipendTypeService.java
@@ -0,0 +1,82 @@
+package com.lxc.service.compensation.stipendtype.impl;
+
+import java.util.List;
+import javax.annotation.Resource;
+import org.springframework.stereotype.Service;
+import com.lxc.dao.DaoSupport;
+import com.lxc.entity.Page;
+import com.lxc.util.PageData;
+import com.lxc.service.compensation.stipendtype.StipendTypeManager;
+
+/** 
+ * 说明： 薪资标准
+ * 创建人：lxc Q1094921525
+ * 创建时间：2018-03-22
+ * @version
+ */
+@Service("stipendtypeService")
+public class StipendTypeService implements StipendTypeManager{
+
+	@Resource(name = "daoSupport")
+	private DaoSupport dao;
+	
+	/**新增
+	 * @param pd
+	 * @throws Exception
+	 */
+	public void save(PageData pd)throws Exception{
+		dao.save("StipendTypeMapper.save", pd);
+	}
+	
+	/**删除
+	 * @param pd
+	 * @throws Exception
+	 */
+	public void delete(PageData pd)throws Exception{
+		dao.delete("StipendTypeMapper.delete", pd);
+	}
+	
+	/**修改
+	 * @param pd
+	 * @throws Exception
+	 */
+	public void edit(PageData pd)throws Exception{
+		dao.update("StipendTypeMapper.edit", pd);
+	}
+	
+	/**列表
+	 * @param page
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public List<PageData> list(Page page)throws Exception{
+		return (List<PageData>)dao.findForList("StipendTypeMapper.datalistPage", page);
+	}
+	
+	/**列表(全部)
+	 * @param pd
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public List<PageData> listAll(PageData pd)throws Exception{
+		return (List<PageData>)dao.findForList("StipendTypeMapper.listAll", pd);
+	}
+	
+	/**通过id获取数据
+	 * @param pd
+	 * @throws Exception
+	 */
+	public PageData findById(PageData pd)throws Exception{
+		return (PageData)dao.findForObject("StipendTypeMapper.findById", pd);
+	}
+	
+	/**批量删除
+	 * @param ArrayDATA_IDS
+	 * @throws Exception
+	 */
+	public void deleteAll(String[] ArrayDATA_IDS)throws Exception{
+		dao.delete("StipendTypeMapper.deleteAll", ArrayDATA_IDS);
+	}
+	
+}
+
